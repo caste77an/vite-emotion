7d52cc94c4e127eff1b54fb5b0298ac586dcae48
caste77an <hancastellan@naver.com>
2025-02-26 19:22:16 +0900

    remove app.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom'
+import { RootRouter } from './page/router.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <RootRouter />
+  </BrowserRouter>
 )
---
+++ src/page/dashboard/index.tsx
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled'
+
+export const Dashboard = () => {
+    return (
+        <StyledP>
+            <p>dashboard page</p>
+        </StyledP>
+    )
+}
+
+const StyledP = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+`
